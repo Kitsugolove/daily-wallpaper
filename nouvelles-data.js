b24bcc667a4a6fdff75854681e32f06c544bbebb
daily-wallpaper <Gaspard.cookiclic@outlook.fr>
2026-09-11 13:19:15 +0200

    Ajout de la page Nouvelles

---
+++ nouvelles-data.js
@@ -0,0 +1,16 @@
+const nouvelles = [
+  {
+    badge: "Nouveauté",
+    date: "11/09/2026",
+    titre: "Lancement des collections dynamiques",
+    texte: "Nous avons le plaisir d'annoncer que nos collections s'adaptent désormais à la saison. Attendez-vous à voir plus de couleurs chaudes cet automne !",
+    categorie: "nouveaute"
+  },
+  {
+    badge: "Nouveauté",
+    date: "06/09/2026",
+    titre: "La future mascotte !",
+    texte: "Bonjour à vous chers abonnés ! Je vous annonce l'arrivée de Kitsu, la mascotte de la chaîne !",
+    categorie: "nouveaute"
+  }
+];
